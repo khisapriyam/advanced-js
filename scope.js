ec37a732d7940d44c63b41385321cb8b4bd0a6e1
khisapriyam <55364142+khisapriyam@users.noreply.github.com>
2020-08-07 15:54:20 +0200

    final chnage

---
+++ scope.js
@@ -0,0 +1,19 @@
+let bonus = 20;
+
+
+function sum (first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    if(result > 9){
+        let mood = "happy";
+        mood = "fishy"
+        mood = "cranky"
+        console.log(mood);
+
+    }
+    // console.log(mood);
+    return result;
+}
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
